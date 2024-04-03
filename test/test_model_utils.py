bd626d3109e0fd5adcb05ff7501cd6b5bd157bb9
elcoxo <38883294+elcoxo@users.noreply.github.com>
2024-04-03 11:15:44 +0300

    lab1 setup

---
+++ test/test_model_utils.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import pytest
+import pandas as pd
+from model_utils import make_inference, load_model
+from sklearn.pipeline import Pipeline
+from pickle import dumps
+
+
+@pytest.fixture
+def create_data() -> dict[str, int | float]:
+    return {"test": 'hi my name is slim shady'}
+
+
+def test_make_inference(monkeypatch, create_data):
+    def mock_get_predictions(_, data: pd.DataFrame) -> list[list[float]]:
+        assert create_data == {
+            key: value[0] for key, value in data.to_dict("list").items()
+        }
+        return {"IE": 1.0,
+                "NS": 1.0,
+                "TF": 0.0,
+                "JP": 0.0
+                }
+
+    in_model = Pipeline([])
+    monkeypatch.setattr(Pipeline, "predict", mock_get_predictions)
+
+    result = make_inference(in_model, create_data)
+    assert result == {"IE": 1.0,
+                "NS": 1.0,
+                "TF": 0.0,
+                "JP": 0.0
+                }
+
+
+@pytest.fixture()
+def filepath_and_data(tmpdir):
+    p = tmpdir.mkdir("datadir").join("fakedmodel.pkl")
+    example: str = "Test message!"
+    p.write_binary(dumps(example))
+    return str(p), example
+
+
+def test_load_model(filepath_and_data):
+    assert filepath_and_data[1] == load_model(filepath_and_data[0])
